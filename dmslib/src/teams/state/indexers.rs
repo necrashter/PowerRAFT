82b8269596fcfa712d653b7ac94383c652b55562
necrashter <iiilker99@gmail.com>
2022-12-29 16:23:42 +0300

    Start implementing BTreeMap (not added to solve_variations or tests yet)

--- dmslib/src/teams/state/indexers.rs
+++ dmslib/src/teams/state/indexers.rs
@@ -1,4 +1,5 @@
 use super::*;
+use std::collections::BTreeMap;
 
 /// A trait for indexing the explored states of a team-based restoration problem.
 pub trait StateIndexer {
@@ -9,7 +10,7 @@ pub trait StateIndexer {
     /// Get the index of given state, adding it to the hasmap when necessary.
     fn index_state(&mut self, s: State) -> usize;
     /// Get the state at given index.
-    fn get_state(&self, index: usize) -> State;
+    fn get_state(&mut self, index: usize) -> State;
     /// Deconstruct the state indexer to state space.
     fn deconstruct(self) -> (Array2<BusState>, Array2<TeamState>);
 }
@@ -59,7 +60,7 @@ impl StateIndexer for NaiveStateIndexer {
         }
     }
 
-    fn get_state(&self, index: usize) -> State {
+    fn get_state(&mut self, index: usize) -> State {
         State {
             buses: self.bus_states.row(index).to_vec(),
             teams: self.team_states.row(index).to_vec(),
@@ -68,6 +69,81 @@ impl StateIndexer for NaiveStateIndexer {
 
     fn deconstruct(self) -> (Array2<BusState>, Array2<TeamState>) {
         (self.bus_states, self.team_states)
+    }
+}
+
+/// A state indexer based on BTreeMap.
+pub struct BTreeStateIndexer {
+    bus_count: usize,
+    team_count: usize,
+    btree: BTreeMap<State, usize>,
+    stack: Vec<State>,
+}
+
+impl StateIndexer for BTreeStateIndexer {
+    fn new(bus_count: usize, team_count: usize) -> Self {
+        BTreeStateIndexer {
+            bus_count,
+            team_count,
+            btree: BTreeMap::new(),
+            stack: Vec::new(),
+        }
+    }
+
+    fn get_state_count(&self) -> usize {
+        self.btree.len()
+    }
+
+    fn index_state(&mut self, s: State) -> usize {
+        match self.btree.get(&s) {
+            Some(i) => *i,
+            None => {
+                let i = self.btree.len();
+                self.stack.push(s.clone());
+                self.btree.insert(s, i);
+                i
+            }
+        }
+    }
+
+    /// NOTE: INDEX IS IGNORED!
+    /// A stack is maintained in which newly indexed states are added. This returns states from
+    /// that stack.
+    fn get_state(&mut self, _index: usize) -> State {
+        self.stack.pop().expect("State stack is empty in get_state")
+    }
+
+    fn deconstruct(self) -> (Array2<BusState>, Array2<TeamState>) {
+        let BTreeStateIndexer {
+            bus_count,
+            team_count,
+            btree,
+            stack,
+        } = self;
+        if stack.len() != 0 {
+            panic!("State stack is not empty in deconstruct");
+        }
+        drop(stack);
+        let state_count = btree.len();
+        let mut bus_states = Array2::default((state_count, bus_count));
+        let mut team_states = Array2::default((state_count, team_count));
+        for (state, i) in btree.into_iter() {
+            for (x, y) in bus_states
+                .row_mut(i)
+                .iter_mut()
+                .zip(state.buses.into_iter())
+            {
+                *x = y;
+            }
+            for (x, y) in team_states
+                .row_mut(i)
+                .iter_mut()
+                .zip(state.teams.into_iter())
+            {
+                *x = y;
+            }
+        }
+        (bus_states, team_states)
     }
 }
 
@@ -92,7 +168,7 @@ impl StateIndexer for SortedStateIndexer {
     }
 
     #[inline]
-    fn get_state(&self, index: usize) -> State {
+    fn get_state(&mut self, index: usize) -> State {
         self.0.get_state(index)
     }
 
