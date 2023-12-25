204995e2508789c86b74ea7eab5602a703a580bf
necrashter <iiilker99@gmail.com>
2023-12-25 23:22:35 +0300

    Print with colors in dqn commands

--- dmscli/src/commands/dqn.rs
+++ dmscli/src/commands/dqn.rs
@@ -19,11 +19,18 @@ pub struct ModelArgs {
 
 /// Load model and print name information.
 fn load_model(path: PathBuf) -> DqnModel {
-    println!("Loading model: {}", path.display());
     match DqnModel::read_from_file(path) {
         Ok(model) => {
-            println!("Model Name: {}", model.name.as_deref().unwrap_or("-"));
-            println!("Problem Name: {}", model.name.as_deref().unwrap_or("-"));
+            println!(
+                "{:14}{}",
+                "Model Name:".bold(),
+                model.name.as_deref().unwrap_or("-")
+            );
+            println!(
+                "{:14}{}",
+                "Problem Name:".bold(),
+                model.problem.name.as_deref().unwrap_or("-")
+            );
             model
         }
         Err(err) => fatal_error!(1, "Cannot read model info: {}", err),
@@ -41,7 +48,7 @@ impl DqnCommand {
                     trainer,
                 } = load_model(args.path);
 
-                println!("\nStarting training...");
+                println!("\n{}", "Starting training...".green().bold());
 
                 let (problem, config) = match problem.prepare() {
                     Ok(x) => x,
@@ -50,11 +57,20 @@ impl DqnCommand {
                 let mut trainer =
                     trainer.build(&problem.graph, problem.initial_teams.clone(), model, config);
                 let value = trainer.evaluate();
-                println!("value: {value}");
+                println!(
+                    "{:39} | Value: {:20}",
+                    "Evaluation before training".dimmed().bold(),
+                    format!("{}", value).bold(),
+                );
                 for i in 1..=1000 {
                     let loss = trainer.train(500);
                     let value = trainer.evaluate();
-                    println!("Epoch {i}\n\tloss: {loss}\n\tvalue: {value}");
+                    println!(
+                        "{:10} | Loss: {:20} | Value: {:20}",
+                        format!("EPOCH {i}").green().bold(),
+                        format!("{}", loss).bold(),
+                        format!("{}", value).bold(),
+                    );
                 }
             }
             DqnCommand::Run(args) => {
